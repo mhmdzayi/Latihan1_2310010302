add53096388de9c32f9b114d333881f0184fbba6
mhmdzayi <zainimuhmmd023gmail.com>
2025-06-09 20:25:25 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author lenovo
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika zaini = new Matematika(0, 2);
+        
+        System.out.println("Hasil Penjumlahan : "+zaini.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : "+zaini.setPengurangan());
+        System.out.println("Hasil Perkalian   : "+zaini.setPerkalian());
+        System.out.println("Hasil Pembagian   : "+zaini.setPembagian());
+    }   
 }
